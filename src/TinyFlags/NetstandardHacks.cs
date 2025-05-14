491f444bdc2f24109838048157b9664cb09bf982
Bob Davidson <bob@blendinteractive.com>
2025-05-13 20:57:18 -0500

    Added netstandard2.0 support

---
+++ src/TinyFlags/NetstandardHacks.cs
@@ -0,0 +1,10 @@
+﻿#if NETSTANDARD2_0
+// Allows me to use newish C# features in a netstanard 2.0 build.
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal static class IsExternalInit { }
+#endif
